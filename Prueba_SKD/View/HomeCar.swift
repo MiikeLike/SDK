dd2af3a84ae8789bbe802113a38780b24a5d83f0
MiikeLike <mak_valle@hotmail.es>
2024-02-02 11:53:12 +0100

    Added maps to the home section

--- Prueba_SKD/View/HomeCar.swift
+++ Prueba_SKD/View/HomeCar.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct HomeCar: View {
     let primaryColor = Color(#colorLiteral(red: 0, green: 0, blue: 0.2588235294117647, alpha: 1))
     
+    //MARK: Map
+    private struct DefaultRegion {
+        static let latitude = 9.9333
+        static let longitude = -84.0833
+    }
+    
+    private struct Span {
+        static let delta = 0.1
+    }
+    
+    @State var coordinateRegion: MKCoordinateRegion = .init(center: .init(latitude: DefaultRegion.latitude, longitude: DefaultRegion.longitude), span: .init(latitudeDelta: Span.delta, longitudeDelta: Span.delta))
+    
+    
     var body: some View {
-        
         ZStack{
             let colors: [Color] = [primaryColor, .gray]
             LinearGradient(gradient: Gradient(colors: colors), startPoint: .center, endPoint: .bottom)
@@ -21,6 +34,9 @@ struct HomeCar: View {
                     .resizable()
                     .edgesIgnoringSafeArea(.top)
                     .frame(height: 350)
+                Map(coordinateRegion: $coordinateRegion, showsUserLocation: true)
+                    .edgesIgnoringSafeArea(.bottom)
+                    .frame(height: 300)
                 TabView {
                     ButtonTab()
                         .tabItem {
