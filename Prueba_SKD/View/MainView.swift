563a9ff76eea78f93268484a248580ce2f3cc04c
MiikeLike <mak_valle@hotmail.es>
2024-02-05 11:52:36 +0100

    added main view for TabView

---
+++ Prueba_SKD/View/MainView.swift
@@ -0,0 +1,31 @@
+//
+//  MainView.swift
+//  Prueba_SKD
+//
+//  Created by Mikel Valle on 5/2/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView{
+            HomeCar().tabItem {
+                Label("Inicio", systemImage: "house.fill")
+            }
+            
+            CarView().tabItem {
+                Label("Vehículo", systemImage: "car.front.waves.up.fill")
+            }
+            
+            Maintenance().tabItem {
+                Label("Mantenimiento", systemImage:"suv.side.front.open")
+            }
+        }
+        .accentColor(.white)
+    }
+}
+
+#Preview {
+    MainView()
+}
